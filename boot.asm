f2abba84ae3f43f13a27303904a10c3bb70354fa
root <root@localhost.localdomain>
2021-01-28 01:05:51 -0800

    使用 int 10h 打印字符串

--- boot.asm
+++ boot.asm
@@ -10,5 +10,28 @@ LABEL_START:
 	mov ah,	0Ch
 	mov [gs:((80 * 2 + 40) * 2)],	ax
 
+	; 打印字符串
+	mov ax, cs
+	mov es, ax
+	mov ds, ax
+	mov ax, BootMessage
+	;mov bp,	BootMessage
+	mov bp, ax
+	;mov cx,	BootMessageLength
+	;mov cx, 16
+	mov ax, BootMessageLength
+	mov cx, ax
+	mov ax,	1301h
+	mov bx, 000ch
+	mov dl,	10
+	mov dh, 15
+	int 10h
+	
+	jmp $
+
+BootMessage:	db	"Hello,World OS!"
+;BootMessageLength:	db	$ - BootMessage
+BootMessageLength	equ	$ - BootMessage
+
 times	510 - ($ - $$)	db	0
 dw	0xAA55
