b89c1bc35f53dd0a01d60add12229fcc5852bf5f
cg <chuganghong@qq.com>
2021-02-18 20:04:58 +0800

    v1，loader

---
+++ v1/test/loader.asm
@@ -0,0 +1,19 @@
+org	0100h
+
+	mov ax, 0B800h
+	mov gs, ax
+	mov ah, 0Ch
+	mov al, 'M'
+	mov [gs:(80 * 16 + 20)*2], ax
+
+        mov ah, 0Ch
+        mov al, 'M'
+        mov [gs:(80 * 16 + 20)*2], ax
+
+        mov ah, 0Ch
+        mov al, 'M'
+        mov [gs:(80 * 16 + 20)*2], ax
+
+        mov ah, 0Ch
+        mov al, 'M'
+        mov [gs:(80 * 16 + 20)*2], ax
