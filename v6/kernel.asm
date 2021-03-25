d237539d4e3580d73b4e70f523c01fd052e705ca
EricXiao <chuganghong@qq.com>
2021-03-25 09:17:59 -0700

    v6-能够读取最大1.44m的内核并执行

--- v6/kernel.asm
+++ v6/kernel.asm
@@ -3,8 +3,12 @@
 global _start
 
 _start:
-	xchg bx, bx
-	;times	85537	db	0
+	times	131072	db	0
+	times	131072	db	0
+	times	131072	db	0
+	times	131072	db	0
+	times	131072	db	0
+	times	131072	db	0
 	mov ah, 0Fh
 	mov al, 'C'
 	mov [gs:(80 * 20 + 40) * 2], ax
@@ -13,9 +17,9 @@ _start:
 	jmp $
 	jmp $
 
-	times	131072	db	0
-	times	131072	db	0
-	times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
 	;times	131072	db	0
 	;times	131072	db	0
 	;times	131072	db	0
