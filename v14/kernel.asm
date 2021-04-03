574b1cb7ed7837870ac6b41ef776032b3649eeb5
EricXiao <chuganghong@qq.com>
2021-04-02 23:59:06 -0700

    v14--时钟中断频率--init

---
+++ v14/kernel.asm
@@ -0,0 +1,29 @@
+[section .text]
+
+global _start
+
+_start:
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	mov ah, 0Fh
+	mov al, 'C'
+	mov [gs:(80 * 20 + 40) * 2], ax
+	mov [gs:(80 * 21 + 40) * 2], ax
+	mov [gs:(80 * 22 + 80) * 2], ax
+	jmp $
+	jmp $
+
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	131072	db	0
+	;times	85537	db	0
+	;times	85537	db	0
