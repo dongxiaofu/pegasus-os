5f4c5a990e9491ed2aa4a5dc23eecf44c619dc01
EricXiao <chuganghong@qq.com>
2021-04-03 07:52:07 -0700

    v15--重载gdt--代码写完--故障

--- v15/kernel.asm
+++ v15/kernel.asm
@@ -1,5 +1,6 @@
 [section .text]
-
+;extern gdt_ptr
+;extern ReloadGDT
 global _start
 
 _start:
@@ -9,12 +10,19 @@ _start:
 	;times	131072	db	0
 	;times	131072	db	0
 	;times	131072	db	0
-	mov ah, 0Fh
-	mov al, 'C'
+	mov ah, 0Bh
+	mov al, 'L'
 	mov [gs:(80 * 20 + 40) * 2], ax
-	mov [gs:(80 * 21 + 40) * 2], ax
-	mov [gs:(80 * 22 + 80) * 2], ax
-	jmp $
+
+	;sgdt [gdt_ptr]
+	;call ReloadGDT
+	;lgdt [gdt_ptr]
+
+	mov ah, 0Bh
+	mov al, 'M'
+	mov [gs:(80 * 20 + 41) * 2], ax
+
+
 	jmp $
 
 	;times	131072	db	0
