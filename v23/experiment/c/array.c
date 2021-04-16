03cc68208d66a87174daec34c9656771bee9cc44
EricXiao <chuganghong@qq.com>
2021-04-16 16:09:00 -0700

    v23---进程---系统调用

---
+++ v23/experiment/c/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int arr[3] = {0,1,2};
+
+	printf("arr[0] = %d\n", arr[0]);	
+	
+	int *ptr = arr;
+	printf("arr[0] = %d\n", *ptr);
+	ptr++;
+	printf("arr[0] = %d\n", *ptr);
+	return 0;
+}
