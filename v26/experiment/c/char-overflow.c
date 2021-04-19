54ea41c0b1014a2a76d8649b6a43c4dd64d3ce5f
EricXiao <chuganghong@qq.com>
2021-04-19 15:42:50 -0700

    v26--输入输出系统---TTY任务--init

---
+++ v26/experiment/c/char-overflow.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	char ch = 0xfffffff9E;
+	printf("ch = %d\n", ch);
+
+	return 0;
+}
---
+++ v26/string.asm
@@ -0,0 +1,81 @@
+[SECTION .txt]
+
+global Memcpy
+global Memset
+Memcpy:
+	push ebp
+	mov ebp, esp
+	push eax
+	push ecx
+	push esi
+	push edi
+	;mov bp, sp
+	;mov di, [bp + 4]        ; p_vaddr，即 dst
+	;mov si, [bp + 8]        ; p_off，即 src
+	;mov cx, [bp + 12]	; 程序头的个数，即p_size
+
+	;mov di, [bp + 8]        ; p_vaddr，即 dst
+	;mov si, [bp + 12]        ; p_off，即 src
+	;mov cx, [bp + 16]       ; 程序头的个数，即p_size
+
+	mov edi, [ebp + 8]        ; p_vaddr，即 dst
+	mov esi, [ebp + 12]        ; p_off，即 src
+	mov ecx, [ebp + 16]       ; 程序头的个数，即p_size
+	;push es
+
+	; 在32位模式下，这两步操作不需要。而且，我没有找到把大操作数赋值给小存储单元的指令。
+	; mov es, edi
+	; mov edi, 0
+
+.1:
+	mov byte al, [ds:esi]
+	mov [es:edi], al
+
+	inc esi
+	inc edi
+	cmp ecx, 0
+	jz .2
+	dec ecx
+
+	cmp ecx, 0
+	jz .2
+	jmp .1
+
+.2:
+	;pop es
+	mov eax, [ebp + 8]
+
+	pop edi
+	pop esi
+	pop ecx
+	pop eax
+	pop ebp
+
+	ret
+
+; Memset(void *dest, char character, int size)
+Memset:
+	;;;;;xchg bx, bx
+	push ebp
+	mov ebp, esp	
+	
+	push eax
+	
+	mov ecx, [ebp + 16]	; size
+	mov esi, [ebp + 8]	; dest
+	mov byte al, [ebp + 12]	; character	
+
+.1:
+	cmp ecx, 0
+	jz .2
+	mov [esi], al
+	dec ecx
+	inc esi		
+	jmp .1
+.2:
+	;;;;;xchg bx, bx
+	pop eax
+	pop ebp	
+	;mov esp, ebp
+	;;;;;xchg bx, bx
+	ret	
