d32433c89666ac22d0a89e8026eb6242bbb17d0a
EricXiao <chuganghong@qq.com>
2021-04-22 07:12:35 -0700

    v26-1--测试数组地址运算

--- v26-1/experiment/c/array.c
+++ v26-1/experiment/c/array.c
@@ -10,5 +10,25 @@ int main(int argc, char **argv)
 	printf("arr[0] = %d\n", *ptr);
 	ptr++;
 	printf("arr[0] = %d\n", *ptr);
+	printf("arr[2] - arr = %d\n", &arr[2] - arr);
+
+	
+	typedef struct{
+		int age;
+		int height;
+	}Person;
+	
+	Person arr2[3];
+
+	Person p1 = {23, 24};
+	Person p2 = {25, 26};
+	Person p3 = {27, 28};
+
+	arr2[0] = p1;
+	arr2[1] = p2;
+	arr2[2] = p3;	
+
+	printf("arr2[2] - arr2 = %d\n", &arr2[2] - arr2);
+
 	return 0;
 }
