fea745a144b6c9661faf46d756acfe01db6d8423
cg <chuganghong@qq.com>
2021-04-24 08:41:32 +0800

    v26--tty基本写完

--- v26-1/main.c
+++ v26-1/main.c
@@ -310,13 +310,25 @@ void in_process(unsigned int key);
 // TaskTTY start
 #define TTY_BUF_SIZE 800
 #define TTY_NUM 3
+// 一屏占用的显存空间。
+// todo 我不确定是不是整个值。
+#define SCREEN_SIZE 80 * 15
+// 一行的宽度
+#define SCREEN_WIDTH 80
+// 显存的初始地址
+#define VM_BASE_ADDR 0xb8000
+// 显存的总大小
+#define VM_TOTAL (0xBFFFF - 0xb8000)
+// 打印字符的默认颜色
+#define DEFAULT_COLOUR 0x0A
 // 当前tty
-unsigned char current_tty;
+TTY *current_tty;
 
 typedef struct{
 	unsigned int vm_original_addr;
 	unsigned int vm_limit;
 	unsigned int start_video_addr;
+	unsigned int cursor;
 }CONSOLE;
 
 typedef struct{
@@ -330,15 +342,18 @@ typedef struct{
 TTY tty_table[TTY_NUM];
 CONSOLE console_table[TTY_NUM];
 
-void init_tty();
-void init_screen(TTY *tty);
+// 设置console的start_video_addr
+void set_console_start_video_addr(unsigned int start_video_addr);
 void select_console(unsigned char tty_index);
 void put_key(TTY *tty, unsigned char key);
 void scroll_up(TTY *tty);
 void scroll_down(TTY *tty);
 void out_char(TTY *tty, unsigned char key);
-void tty_do_read();
-void tty_do_write();
+void tty_do_read(TTY *tty);
+void tty_do_write(TTY *tty);
+void init_screen(TTY *tty);
+void init_tty();
+
 
 
 // TaskTTY end
@@ -1130,3 +1145,186 @@ void in_process(unsigned int key)
 }
 
 // 键盘 end
+
+// TTY start
+
+void select_console(unsigned char tty_index)
+{
+	if(tty_index <0 || tty_index > TTY_NUM - 1){
+		return;
+	}
+
+	// 不确定这个写法是否能达到预期目的
+	current_tty = &tty_table[tty_index];
+}
+
+
+void set_console_start_video_addr(unsigned int start_video_addr)
+{
+
+}
+
+/*====================================================
+把数据放入tty的缓冲区。逻辑如下：
+1. 缓冲区未满才放入。
+2. 放入head，然后head向后移动一位。
+3. counter++。
+4. 检查，head == tty_buf_table + 缓冲区长度。
+5. 如果上面的条件成立，重置 head = tty_buf_table。
+======================================================*/
+void put_key(TTY *tty, unsigned char key)
+{
+	if(tty->counter > 0){
+		*(tty->head) = key;
+		tty->head++;
+		tty->counter++;
+		if(tty->head == tty->tty_buf_table + KEYBOARD_BUF_SIZE){
+			tty->head = tty->tty_buf_table;
+		}
+	}
+}
+
+/*====================================================
+把tty的屏幕向上滚动一屏。逻辑如下：
+1. 如果终端的start_video_addr > original_addr，那么，可以向上滚屏。
+2. start_video_addr 是 SCREEN_SIZE 的倍数。
+3. 滚屏，涉及到寄存器操作。
+======================================================*/
+void scroll_up(TTY *tty)
+{
+	if(tty->console->start_video_addr > tty->console->original_addr){
+		set_console_start_video_addr(tty->console->start_video_addr - SCREEN_SIZE);
+	}
+}
+
+/*====================================================
+把tty的屏幕向下滚动一屏。逻辑如下：
+1. 剩余的显存大于一屏的空间。
+2. 记得不牢，需要再次分析、计算。
+3. 剩余的显存大于一屏的空间，怎么写代码？
+======================================================*/
+void scroll_down(TTY *tty)
+{
+	if(tty->console->start_video_addr + 2 * SCREEN_SIZE < tty->console->original_addr + tty->console->limit){
+		set_console_start_video_addr(tty->console->start_video_addr + SCREEN_SIZE);
+	}
+}
+
+/*====================================================
+耗费最多时间。
+把字符写入显存，代替disp_str。逻辑如下：
+1. 换行符。最费劲。我的理解，仍有点不透彻。
+2. 退格符。
+3. 普通字符。
+======================================================*/
+void out_char(TTY *tty, unsigned char key)
+{
+	unsigned int addr_in_vm = VM_BASE_ADDR + tty->console->cursor * 2;
+
+	switch(key){
+		case '\n':
+			// 换行
+			// 看了于上神的代码，我才写出来。唉-----
+			if(tty->console->cursor < tty->console->original_addr + tty->console->vm_limit - SCREEN_WIDTH){
+				tty->console->cursor = tty->console->original_addr + 
+				((tty->console->cursor - tty->console->original_addr)/SCREEN_WIDTH + 1) * SCREEN_WIDTH;
+			}
+			break;
+		case '\b':
+			// 退格
+			//if(tty->console->cursor > 0){
+			if(tty->console->cursor > tty->console->original_addr){
+				*(addr_in_vm - 2) = ' ';
+				*(addr_in_vm - 1) = DEFAULT_COLOUR;
+				tty->console->cursor--;
+			}
+			break;
+		default:
+			//普通字符
+			// 也看了于上神的代码。这个分支，我完全能独立写出来。不要依赖别人代码。
+			// 总看别人的代码，那么，这是我自己写的还是抄别人的？
+			// 实在实在想不出来，再去看别人的。
+			// 这个分支，完全只是普通的逻辑，没有任何领域知识。我应该完全能够独立写出来。
+			if(tty->console->cursor + 1 < tty->console->original_addr + tty->console->vm_limit){
+				*(addr_in_vm + 1) = key;
+				*(addr_in_vm + 2) = DEFAULT_COLOUR;
+				tty->console->cursor++;
+			}
+			break;
+	}
+
+	// 向下滚屏
+	// 什么时候需要滚屏？我又不记得了。独立分析出来！
+	// 超过一屏数据时，需要滚屏。
+	while(tty->console->cursor - tty->console->start_video_addr > SCREEN_SIZE){
+		scroll_down(tty);
+	}
+}
+
+/*====================================================
+把数据从键盘缓冲区读出来放到tty缓冲区。逻辑如下：
+1. 键盘缓冲区有数据才读。
+2. 遇到ENTER转换成\n。
+3. 遇到BACKSPACE转换成\b。
+3. 用put_key放入tty缓冲区。
+======================================================*/
+void tty_do_read(TTY *tty)
+{
+	// if条件，似乎可以不要
+	if(keyboard_buffer.counter > 0){
+		keyboard_read();
+	}
+}
+
+/*====================================================
+把数据从tty缓冲区读出来写到显存。逻辑如下：
+
+======================================================*/
+void tty_do_write(TTY *tty)
+{
+	while(tty->console->counter > 0){
+		unsigned char key = *(tty->console->tail);
+		tty->console->tail++;
+		tty->console->counter--;
+		if(tty->console->tail == tty->buf + KEYBOARD_BUF_SIZE){
+			tty->console->tail == tty->buf
+		}
+		out_char(tty, key);
+	}
+}
+
+/*====================================================
+初始化CONSOLE。逻辑如下：
+1. 难点是，如何确定original_addr?
+需要确定，是第几个console。
+======================================================*/
+//void init_screen(CONSOLE *console)
+void init_screen(TTY *tty)
+{
+	unsigned int index = tty - tty_table;
+	CONSOLE *console = &console_table[index];
+
+	// 单位转换为字
+	console->vm_limit = (VM_TOTAL / TTY_NUM) >> 1;
+
+	console->original_addr = VM_BASE_ADDR + index * console->vm_limit;
+
+	console->cursor = console->start_video_addr = console->original_addr;
+}
+
+/*====================================================
+初始tty。逻辑如下：
+
+======================================================*/
+void init_tty()
+{
+	for(TTY *tty = tty_table; tty < tty_table + TTY_NUM; tty++){
+		// 初始化缓冲区
+		tty->head = tty->tail = tty->buf;
+		tty->counter = 0;
+		// 初始化console
+		init_screen(tty);
+	}
+}
+
+// TTY end
