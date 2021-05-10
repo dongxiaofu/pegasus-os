c4c71157d7725cc6813c49a4ffa3086810103c5e
cg <chuganghong@qq.com>
2021-05-10 18:50:55 +0800

    v30--ipc函数写完

--- v30/syscall.asm
+++ v30/syscall.asm
@@ -1,11 +1,15 @@
 global get_ticks
 global write
 global write_debug
+global send_msg
+global receive_msg
 
 [section .data]
 _NR_GET_TICKS	equ	0
 _NR_WRITE	equ	1
 _NR_WRITE_DEBUG	equ	2
+_NR_SEND_MSG	equ		3
+_NR_RECEIVE_MSG	equ		4
 INT_VECTOR_TICKS	equ	0x90
 
 [section .text]
@@ -36,6 +40,31 @@ write_debug:
 	xchg bx, bx
 	mov eax, _NR_WRITE_DEBUG
 	;mov eax, _NR_WRITE
+	; 第2个参数
+	mov ebx, [esp+8]
+	; 第1个参数
+	mov ecx, [esp+4]
+
+	int INT_VECTOR_TICKS
+
+	ret
+
+; int send_msg(Message *msg, int receiver_pid)
+send_msg:
+	mov eax, _NR_SEND_MSG
+	; 第2个参数
+	mov ebx, [esp+8]
+	; 第1个参数
+	mov ecx, [esp+4]
+
+	int INT_VECTOR_TICKS
+
+	ret
+
+
+; int receive_msg(Message *msg, int sender_id)
+receive_msg:
+	mov eax, _NR_RECEIVE_MSG
 	; 第2个参数
 	mov ebx, [esp+8]
 	; 第1个参数
