376b583a019cff624c1384722576f5bc0bb0095d
EricXiao <chuganghong@qq.com>
2021-06-19 01:08:29 -0700

    v31--修改文件头和Makefile

--- v31/hd.c
+++ v31/hd.c
@@ -1,10 +1,12 @@
-#include "main.h"
+// #include "main.h"
 
 // 硬盘驱动
 //
 void TaskHD()
 {
 	Printf("%s\n", "HD driver is running!");
+	char *buf = (char *)(0x475);
+	Printf("Num:%d\n", *buf);
 
 	while(1){
 
