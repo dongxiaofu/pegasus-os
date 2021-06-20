cef440e77d8ce1963ddb680b11e107634d6be426
EricXiao <chuganghong@qq.com>
2021-06-19 23:51:30 -0700

    v31--结构调整--错误很多

--- v31/hd.c
+++ v31/hd.c
@@ -1,14 +1,14 @@
-#include "const.h"
 #include "stdio.h"
 #include "string.h"
+#include "const.h"
+#include "type.h"
 #include "protect.h"
 #include "process.h"
 #include "keymap.h"
 #include "keyboard.h"
 #include "console.h"
-#include "global.h"
 #include "proto.h"
-
+#include "global.h"
 // 硬盘驱动
 void TaskHD()
 {
--- v31/include/sys/global.h
+++ v31/include/sys/global.h
@@ -11,20 +11,22 @@ unsigned int counter;
 // 全局相关 end
 
 // 进程相关 start
+// 即将或正在执行的进程的进程表
+Proc *proc_ready_table;
 // 系统任何和用户进程的进程表都存储在这个数组中
 Proc proc_table[TASK_PROC_NUM + USER_PROC_NUM];
 // 用户进程元数据
 Task user_task_table[USER_PROC_NUM] = {
         {TestA, A_STACK_SIZE},
         {TestB, B_STACK_SIZE},
-        {TestC, C_STACK_SIZE},
+        {TestC, C_STACK_SIZE}
 };
 // 系统任务元数据
 Task sys_task_table[TASK_PROC_NUM] = {
         {TaskTTY, TaskTTY_STACK_SIZE},
         {TaskSys, TASK_SYS_SIZE},
         {TaskHD, TASK_HD_SIZE},
-        {task_fs, TASK_FS_SIZE},
+        {task_fs, TASK_FS_SIZE}
 };
 
 // 进程的堆栈
