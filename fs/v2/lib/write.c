09eff435f3ac0b02f4078dc0290db24517386e6e
EricXiao <chuganghong@qq.com>
2021-07-29 20:13:07 -0700

    fs/v2--open、read、write、unlink库函数

---
+++ fs/v2/lib/write.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "string.h"
+#include "const.h"
+#include "type.h"
+#include "protect.h"
+#include "process.h"
+#include "keyboard.h"
+#include "console.h"
+#include "proto.h"
+#include "global.h"
+
+int write(int fd, const void *buf, int count)
+{
+	Message msg;
+
+	msg.type = WRITE;
+	msg.FD = fd;
+	msg.BUF = buf;
+	msg.CNT = count;
+	
+	send_rec(BOTH, &msg, TASK_FS);
+
+	return msg.CNT;
+}
