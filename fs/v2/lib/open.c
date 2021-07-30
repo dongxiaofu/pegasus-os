09e7b432ae001d3ddfacdd6d8e7c422a51cea882
EricXiao <chuganghong@qq.com>
2021-07-29 20:13:07 -0700

    fs/v2--open、read、write、unlink库函数

---
+++ fs/v2/lib/open.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "string.h"
+#include "const.h"
+#include "type.h"
+#include "protect.h"
+#include "process.h"
+#include "keyboard.h"
+#include "console.h"
+#include "proto.h"
+#include "global.h"
+
+int open(const char *pathname, int flags)
+{
+	Message msg;
+
+	msg.type = OPEN;
+	msg.PATHNAME = (void *)pathname;
+	msg.FLAGS = flags;
+	msg.NAME_LEN = Strlen(pathname);
+	
+	send_rec(BOTH, &msg, TASK_FS);
+
+	assert(msg.type == SYSCALL_RET);
+
+	return msg.FD;
+}
