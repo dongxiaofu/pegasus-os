5d808fa7a959aea9431af734e0aa53c87636bd98
EricXiao <chuganghong@qq.com>
2021-08-07 05:27:59 -0700

    fs/v2-lib/exec、exit、fork、getpid、wait

---
+++ fs/v2/lib/fork.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "string.h"
+#include "const.h"
+#include "type.h"
+#include "protect.h"
+#include "process.h"
+#include "keyboard.h"
+#include "console.h"
+#include "proto.h"
+#include "global.h"
+
+
+int fork()
+{
+	Message msg;
+
+	msg.type = FORK;
+	send_rec(BOTH, &msg, TASK_FS);
+
+	assert(msg.type = SYSCALL_RET);
+	assert(msg.RETVAL == 0);
+
+	return msg.PID;
+}
---
+++ fs/v2/lib/wait.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "string.h"
+#include "const.h"
+#include "type.h"
+#include "protect.h"
+#include "process.h"
+#include "keyboard.h"
+#include "console.h"
+#include "proto.h"
+#include "global.h"
+
+
+int wait(int *status)
+{
+	Message msg;
+
+	msg.type = WAIT;
+	send_rec(BOTH, &msg, TASK_MM);
+
+	*status = msg.STATUS; 
+
+	return (msg.PID == NO_TASK ? -1: msg.PID);
+}
