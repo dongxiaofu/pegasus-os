76a66446ea0582285416867189b14afd60d7784e
dongxiaofu <chuganghong@qq.com>
2022-09-25 10:15:02 +0800

    mem/v10-新增fork代码，OS能运行

--- mem/v10/my_process.c
+++ mem/v10/my_process.c
@@ -57,6 +57,140 @@ void user_process(Func func, void *arg)
 	restart((unsigned int)process_stack);
 }
 
+Proc *clone_pcb(Proc *parent_process)
+{
+	Proc *process = alloc_memory(1, KERNEL);
+	Memcpy(process, parent_process, sizeof(Proc));
+
+	process->pid = ++pid;
+	process->parent_pid = parent_process->parent_pid;
+	unsigned int stack_offset = (unsigned int)(parent_process + PAGE_SIZE) - (unsigned int)parent_process->stack;
+	process->stack = (unsigned int)(process + PAGE_SIZE) - stack_offset;
+
+	// 子进程必须有独立的页表和虚拟地址空间
+	// 建立进程的页表
+	unsigned int page_dir_vaddr = alloc_memory(1, KERNEL);
+	Memcpy((void *)(page_dir_vaddr + 0x300 * 4), (void *)(0xFFFFF000 + 0x300 * 4), 1024); 
+	unsigned int page_dir_phy_addr = get_physical_address(page_dir_vaddr);
+	unsigned int *ptr1 = (unsigned int *)(page_dir_vaddr + 1023*4);
+	*ptr1 = page_dir_phy_addr | PG_P_YES | PG_RW_RW | PG_US_SUPER;
+	process->page_directory = page_dir_phy_addr;
+
+	// 用户进程的虚拟地址空间
+	process->user_virtual_memory_address = (unsigned int)create_user_process_address_space();
+	Memcpy(process->user_virtual_memory_address,parent_process->user_virtual_memory_address,	\
+		sizeof(VirtualMemoryAddress)); 
+
+// 我担心克隆子进程的所有流程还没有完成就调度到子进程了。
+//	// 加入链表，在调度模块中处理
+//	if(process->p_flag == RUNNING){
+//		Memset(&process->tag, 0, sizeof(process->tag));
+//		appendToDoubleLinkList(&pcb_list, (ListElement *)(&process->tag));	
+//	}
+//
+//	Memset(&process->all_tag, 0, sizeof(process->all_tag));
+//	appendToDoubleLinkList(&all_pcb_list, (ListElement *)(&process->all_tag));	
+
+	return process;
+}
+
+void build_body_stack(Proc *parent_process, Proc *child_process, unsigned int buf)
+{
+	VirtualMemoryAddress *virtualMemory = (VirtualMemoryAddress *)parent_process->user_virtual_memory_address;
+	Bitmap bitmap = virtualMemory->map;	
+	char *bits = bitmap.bits;
+	unsigned int map_length = bitmap.length;
+	unsigned int vaddr_start = virtualMemory->start_addr;
+
+	// int bit_idx = 15;
+	int bit_idx = 0;
+	int k = 0;
+	//		disable_int();
+	for(int i = 0; i < map_length; i++){
+		char one_byte = bits[i];
+		for(int j = 0; j < 8; j++){
+		// for(int j = 0; j < 7; j++){
+			// 把if写成这样，是因为我不喜欢在一个花括号中写一大段代码。
+			// if(one_byte & (1 << j) == 0)	{ bit_idx++ ; continue; }
+			if((one_byte & (1 << j)) == 0)
+			{ 
+				bit_idx++ ;
+				 continue;
+			}
+	//		bit_idx++;
+	//		continue;
+			// 把页表更换为父进程的页表
+			update_cr3(parent_process->page_directory);
+			// 当前位是1，它对应的虚拟地址被使用，这个虚拟地址对应的物理空间存储了数据。
+			unsigned int vaddr = vaddr_start + bit_idx * PAGE_SIZE; 
+			// unsigned int vaddr = vaddr_start + bit_idx * PAGE_SIZE - 1; 
+			if(vaddr == 0x8056000){
+				disp_int(vaddr);
+			}
+			// 把起点是vaddr的页框中的数据复制到内核的buf中。
+			Memcpy(buf, vaddr, PAGE_SIZE);
+			
+			// 把页表更换为子进程的页表
+			update_cr3(child_process->page_directory);
+			// 在子进程的页表中建立vaddr和一个物理地址之间的映射关系。
+			get_a_virtual_page(USER, vaddr);
+			// 把buf中的数据复制到子进程中起点是vaddr的页框中。
+			Memcpy(vaddr, buf,  PAGE_SIZE);
+			// 把页表更换为父进程的页表
+			// update_cr3(parent_process->page_directory);
+			bit_idx++;
+			k++;
+		}
+	}	
+
+	disp_str("k = ");
+	disp_int(k);
+	disp_str("\n");
+	//		enable_int();
+}
+
+// 不理解这个函数中的代码。
+void build_process_kernel_stack(Proc *process)
+{
+	unsigned int *stack = (unsigned int *)(process + PAGE_SIZE - sizeof(Regs));
+//	unsigned int *stack = (unsigned int *)((unsigned int)process + PAGE_SIZE - sizeof(Regs));
+	
+	// 这种corner case，我不喜欢处理。
+	unsigned int *eip_in_thread_stack = stack - 0;
+	unsigned int *esi_in_thread_stack = stack - 1;
+	unsigned int *edi_in_thread_stack = stack - 2;
+	unsigned int *ebx_in_thread_stack = stack - 3;
+	unsigned int *ebp_in_thread_stack = stack - 4;
+
+	*eip_in_thread_stack = restart;
+
+	*esi_in_thread_stack = *edi_in_thread_stack = \
+	*ebx_in_thread_stack = *ebp_in_thread_stack = 0;
+
+	process->stack = stack;
+}
+
+Proc *fork_process(unsigned int parent_pid)
+{
+	Proc *parent_process = pid2proc(parent_pid);
+	Proc *child_process = clone_pcb(parent_process);
+	unsigned int buf = alloc_memory(1, KERNEL);
+	build_body_stack(parent_process, child_process, buf);
+	build_process_kernel_stack(child_process);
+
+	// 加入链表，在调度模块中处理
+	if(child_process->p_flag == RUNNING){
+		Memset(&child_process->tag, 0, sizeof(child_process->tag));
+		appendToDoubleLinkList(&pcb_list, (ListElement *)(&child_process->tag));	
+	}
+
+	Memset(&child_process->all_tag, 0, sizeof(child_process->all_tag));
+	appendToDoubleLinkList(&all_pcb_list, (ListElement *)(&child_process->all_tag));	
+
+	// 在函数返回前，这个新创建的进程会被调度到吗？
+	return child_process;
+}
+
 void process_execute(Func func, char *thread_arg, char *process_name)
 {
 	Proc *process = thread_init();
